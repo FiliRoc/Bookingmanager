f5fbf806da40258286e09158cac1ad3307d110b8
Filip Roček <rocek.filip@szenam.cz>
2024-02-15 21:25:48 +0100

    Initial commit

---
+++ src/Guest.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Guest {
+
+    private String name;
+    private LocalDate birthdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+}
